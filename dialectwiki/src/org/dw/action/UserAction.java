abbc545b1841a013063575cedbb1724776179a39
richard <richard@localhost>
2011-06-08 13:52:46 +0800

    ??:??action????

---
+++ dialectwiki/src/org/dw/action/UserAction.java
@@ -0,0 +1,8 @@
+package org.dw.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+//user Details的action
+public class UserAction extends ActionSupport{
+	
+}
---
+++ dialectwiki/src/org/dw/action/UserIndexAction.java
@@ -0,0 +1,11 @@
+package org.dw.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+//user首页的action
+public class UserIndexAction extends ActionSupport {
+	public String execute()
+	{
+		return SUCCESS;
+	}
+}
