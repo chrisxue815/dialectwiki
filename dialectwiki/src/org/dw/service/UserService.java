e4af780e2aa0df949eaa1cfb2c776acd7a36f6c2
xc <xc@localhost>
2011-06-25 01:42:51 +0800

    ???????????????

---
+++ dialectwiki/src/org/dw/service/UserService.java
@@ -0,0 +1,19 @@
+package org.dw.service;
+
+import java.util.List;
+import java.util.Set;
+
+import org.dw.model.Pronunciation;
+import org.dw.model.User;
+
+public interface UserService
+{
+  public void signup(User user);
+  public User getUserByUserName(String userName);
+  public Set<Pronunciation> getUserProns(String userName);
+  public List<User> getNBUsers(int listSize);
+  public List<User> getNBUsers();
+  public User getUserById(int wordId);
+  public List<User> getGOODUsers(int listSize);
+  public List<User> getGOODUsers();
+}
