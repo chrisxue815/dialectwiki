b375a05e10d2e57cae569fef24b2b7ed7c029238
richard <richard@localhost>
2011-06-28 15:58:16 +0800

    ??:????

--- dialectwiki/src/org/dw/action/DeletePronAction.java
+++ dialectwiki/src/org/dw/action/DeletePronAction.java
@@ -10,7 +10,7 @@ public class DeletePronAction extends ActionSupport {
 	private static final long serialVersionUID = -7156956207442932280L;
 	private static final String INVALID_ID = "invalidid";
 	private static final String ID_NOT_EXIST = "idNotExist";
-	private static final String WORD_NOT_EXIST = "wordNotExist";
+	private static final String PRON_NOT_EXIST = "pronNotExist";
 	private static final String WORDS = "words";
 	
 	private String id;
@@ -18,11 +18,67 @@ public class DeletePronAction extends ActionSupport {
 	private PronunciationService pronunciationService;
 	
 	
+	
+	public String getId() {
+		return id;
+	}
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+	public Pronunciation getPron() {
+		return pron;
+	}
+
+
+
+	public void setPron(Pronunciation pron) {
+		this.pron = pron;
+	}
+
+
+
+	public PronunciationService getPronunciationService() {
+		return pronunciationService;
+	}
+
+
+
+	public void setPronunciationService(PronunciationService pronunciationService) {
+		this.pronunciationService = pronunciationService;
+	}
+
+
+
+
+
+
+
 	public String execute()
 	{
 		try
 		{
-			return null;
+			if( id != null && id !="")
+			{
+				int pronId = Integer.parseInt(id);
+				Pronunciation pron = pronunciationService.getPronunciationById(pronId);
+				
+				if(pron == null)
+					return PRON_NOT_EXIST;
+				
+				pronunciationService.deletePron(pron);
+				
+			}
+			else
+			{
+				return WORDS;
+			}
+			return SUCCESS;
 		}
 		catch(Exception ex)
 		{
@@ -30,5 +86,4 @@ public class DeletePronAction extends ActionSupport {
 			return INVALID_ID;
 		}
 	}
-
 }
