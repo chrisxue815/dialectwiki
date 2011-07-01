ba10be9b1e17b7379d2e383ef3212bad7c65b811
Chris Xue <chrisxue815@gmail.com>
2011-07-01 21:02:07 +0800

    ??:MD5??

--- dialectwiki/src/org/dw/utils/MD5.java
+++ dialectwiki/src/org/dw/utils/MD5.java
@@ -4,12 +4,12 @@ import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 
 public class MD5
 {
-
+  static final String SALT = "hitchimpdw";
+  
   static Md5PasswordEncoder encoder = new Md5PasswordEncoder();
 
   public static String toMD5(String source)
   {
-    return encoder.encodePassword(source, null);
+    return encoder.encodePassword(source, SALT);
   }
-
 }
